50a50368914c63587dc519a562278aeb10bd0db0
Kiyo <88574877+Kyxzy@users.noreply.github.com>
2021-11-20 14:59:24 +0700

    Create nginx-modsec.sh

---
+++ nginx-modsec.sh
@@ -0,0 +1,4 @@
+apt update && apt upgrade -y
+apt-get install bison build-essential ca-certificates curl dh-autoreconf doxygen flex gawk git iputils-ping libcurl4-gnutls-dev libexpat1-dev libgeoip-dev liblmdb-dev libpcre3-dev libpcre++-dev libssl-dev libtool libxml2 libxml2-dev libyajl-dev locales lua5.3-dev pkg-config wget zlib1g-dev zlibc libxslt-dev libgd-dev git -y
+git clone https://github.com/SpiderLabs/ModSecurity.git
+git clone https://github.com/SpiderLabs/ModSecurity-nginx.git
